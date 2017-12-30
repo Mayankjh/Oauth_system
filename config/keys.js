c147bd8da33fdd52058bcbe7a811a5a0d3210ef3
Mayank Jha <mayankjha7722@gmail.com>
2017-12-30 12:42:13 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   google: {
-    clientID: '317247411028-prs3f1vidjgru0rsicr150f3oag5b31t.apps.googleusercontent.com',
-    clientSecret: 'QiVk2qZsxkOszOTwswq1L47x'
+    clientID: '',
+    clientSecret: ''
   },
   mongodb: {
-    dbURI: 'mongodb://oauth_user:oauth_pass@ds235877.mlab.com:35877/oauth_users'
+    dbURI: ''
   },
   session: {
     cookieKey: 'jfvknfvjkfbvsvnsjkvnjvdkvkjvddjv'
